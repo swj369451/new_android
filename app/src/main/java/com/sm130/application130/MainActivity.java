21305612347a4715f51585de87690066c0c05e20
swj369451 <swj369451@163.com>
2019-05-13 21:53:29 +0800

    修改左边侧边栏

--- app/src/main/java/com/sm130/application130/MainActivity.java
+++ app/src/main/java/com/sm130/application130/MainActivity.java
@@ -87,11 +87,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
+//        else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
